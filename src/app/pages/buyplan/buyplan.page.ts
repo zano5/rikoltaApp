ddea321d2bfb364797e2c9bb3c9da8fb2b7e9b5c
Frans911 <212281972@tut4life.ac.za>
2019-11-28 15:58:35 +0200

    7th Commit

--- src/app/pages/buyplan/buyplan.page.ts
+++ src/app/pages/buyplan/buyplan.page.ts
@@ -17,13 +17,13 @@ export class BuyplanPage implements OnInit {
   ngOnInit() { 
     this.activatedRoute.params.subscribe((data) =>{
      this.pricePlan = data.plan;
-     if(this.pricePlan == 'StandardPlan'){
+     if(this.pricePlan == 'Standard Plan'){
         for (let index = 1; index <= 10; index++) {
           this.planMember.push('Member '+index)
           
         }
      }
-     if(this.pricePlan == 'PremiumPlan'){
+     if(this.pricePlan == 'Premium Plan'){
       for (let index = 1; index <= 15; index++) {
         this.planMember.push('Member '+index)
         
